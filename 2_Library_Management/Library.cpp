901beb3f5a8a892e726c11fb390835f3614156ac
Rajesh Pandit <rashq122@gmail.com>
2025-01-11 22:57:51 +0530

    Updating

--- 2_Library_Management/Library.cpp
+++ 2_Library_Management/Library.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 class book{
     private:
-        static int id;
+        static int idGenerator;
+        int id;
         string title;
         string author;
         string genre;
@@ -16,36 +17,13 @@ class book{
         int borrowerId;
 
     public:
-        book(){
-            id++;
-            title = "";
-            author = "";
-            genre = "";
-            publishedYear = 0;
-            isAvailable = 0;
-            borrowerId = 0;
-        }
-        void addBook(){
-            cout<<"Enter the title of the book: ";
-            cin>>title;
-            cout<<"Enter the author of the book: ";
-            cin>>author;
-            cout<<"Enter the genre of the book: ";
-            cin>>genre;
-            cout<<"Enter the published year of the book: ";
-            cin>>publishedYear;
-            isAvailable = true;
-            borrowerId = 0;
-
-        }
-        book(string titl, string auth, string gen, int pubYear, bool avaib, int bId){
-            id++;
+        book(string titl, string auth, string gen, int pubYear, bool avaib){
+            id = ++idGenerator;
             title = titl;
             author = auth;
             genre = gen;
             publishedYear = pubYear;
             isAvailable = avaib;
-            borrowerId = bId;
         }
         int getId(){
             return id;
@@ -85,27 +63,6 @@ class member{
         int borrowedBookId;
 
     public:
-        member(){
-            rollNo = "";
-            name = "";
-            address = "";
-            phone = "";
-            email = "";
-            borrowedBookId = 0;
-        }
-        void addMember(){
-            cout<<"Enter the roll number of the member: ";
-            cin>>rollNo;
-            cout<<"Enter the name of the member: ";
-            cin>>name;
-            cout<<"Enter the address of the member: ";
-            cin>>address;
-            cout<<"Enter the phone number of the member: ";
-            cin>>phone;
-            cout<<"Enter the email of the member: ";
-            cin>>email;
-            borrowedBookId = 0;
-        }
         member(string roll, string nam, string add, string ph, string em){
             rollNo = roll;
             name = nam;
@@ -149,18 +106,40 @@ class library{
             string genre;
             int publishedYear;
             int avl = true;
-            int brrwId = 0;
             cout<<"Enter the title of the book: ";
-            cin>>title;
+            cin.ignore();
+            getline(cin,title);
             cout<<"Enter the author of the book: ";
-            cin>>author;
+            getline(cin,author);
             cout<<"Enter the genre of the book: ";
-            cin>>genre;
+            getline(cin,genre);
             cout<<"Enter the published year of the book: ";
             cin>>publishedYear;
-            books.push_back(book(title, author, genre, publishedYear, avl, brrwId));
+            books.push_back(book(title, author, genre, publishedYear, avl));
 
         }
+
+
+        void addmember(){
+            string roll;
+            string name;
+            string address;
+            string phone;
+            string email;
+            cout<<"Enter the roll No: ";
+            getline(cin,roll);
+            cout<<"Enter the name of the member: ";
+            getline(cin,name);
+            cout<<"Enter the address of the member: ";
+            getline(cin,address);
+            cout<<"Enter the phone number of the member: ";
+            getline(cin,phone);
+            cout<<"Enter the email of the member: ";
+            getline(cin,email);
+            members.push_back(member(roll,name, address, phone, email));
+        }
+
+
         void displayAllBooks(){
             cout<<endl<<endl<<endl;
             cout<<"-----------------Books in the library-----------------"<<endl<<endl;
@@ -170,36 +149,54 @@ class library{
                 cout<<"Author: "<<books[i].getAuthor()<<endl;
                 cout<<"Genre: "<<books[i].getGenre()<<endl;
                 cout<<"Published Year: "<<books[i].getPublishedYear()<<endl;
+                cout<<endl<<endl;
             }
         }
 
-        void addmember(){
-            string roll;
-            string name;
-            string address;
-            string phone;
-            string email;
-            cout<<"Enter the name of the member: ";
-            cin>>name;
-            cout<<"Enter the address of the member: ";
-            cin>>address;
-            cout<<"Enter the phone number of the member: ";
-            cin>>phone;
-            cout<<"Enter the email of the member: ";
-            cin>>email;
-            members.push_back(member(roll,name, address, phone, email));
+        void findBook(){
+            int BOOKID;
+            string BOOKNAME;
+            cout<<"Enter the Book ID: ";
+            cin>>BOOKID;
+            cin.ignore();
+            cout<<"Enter the Book Title";
+            getline(cin,BOOKNAME);
+            cout<<endl;
+            for(int i=0;i<books.size();i++){
+                int bookId = books[i].getId();
+                string bookName = books[i].getTitle();
+                if(bookId==BOOKID || bookName==BOOKNAME){
+                cout<<"Book ID: "<<books[i].getId()<<endl;
+                cout<<"Title: "<<books[i].getTitle()<<endl;
+                cout<<"Author: "<<books[i].getAuthor()<<endl;
+                cout<<"Genre: "<<books[i].getGenre()<<endl;
+                cout<<"Published Year: "<<books[i].getPublishedYear()<<endl;
+                int availability = books[i].getisAvailable();
+                if(availability){
+                    cout<<"Book Availability: True"<<endl;
+                }
+                else{
+                    cout<<"Book Availability: False"<<endl;
+                }
+                break;
+                }
+            }
         }
+
 
         
 };
 
 
 
-int book::id = 0;
+int book::idGenerator = 0;
 
 int main(){
     library l;
     l.addBook();
+    l.addBook();
+    l.addBook();
     l.displayAllBooks();
+    l.findBook();
 
 }
